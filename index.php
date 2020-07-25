f740f9feb6ec747747c779194cb192e213e5c5e8
zinou3310 <68792944+zinou3310@users.noreply.github.com>
2020-07-25 22:34:26 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<!--
+	Fractal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Bls Cancel</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="content">
+					<h1><a href="#">Bienvenue au site de déblocage RDV -BLS-  BLS مرحبا بكم في موقع الغاء مواعيد  </a></h1>
+					 .... </a></p>
+					<ul class="actions">
+						<li><a href="#one" class="button icon solid fa-chevron-down scrolly">Remplir le formulaire du déblocage - املئ الاستمارة</a></li>
+					</ul>
+				</div>
+				<div class="image phone"><div class="inner"><img src="images/screen.jpg" alt="" /></div></div>
+			</header>
+
+		<!-- One -->
+			<section id="one" class="wrapper style2 special">
+				<header class="major">
+					<h2>Veillez remplire ce formulaire<br />
+					يرجى ملئ الاستمارة</h2><img src="images/screen.jpg"  width="50" height="60">
+											<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSe191eUF2pmQjXZCRS7WFWLWE4sBVKuwGjQ8QIMI68FROernQ/viewform?usp=sf_link" class="button primary icon solid fa-book">Remplir le formulaire du déblocage</a></li>
+
+				</header>
+				</ul>
+			</section>
+			</div>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
+</html>
+
